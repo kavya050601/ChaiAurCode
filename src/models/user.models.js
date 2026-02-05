631e45017e7ad186294669c5a44e4deefddd7559
kavya050601 <kavya050601@gmail.com>
2026-02-05 14:44:44 +0530

    Add refreshAccessToken endpoint

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -51,11 +51,12 @@ const userSchema = new Schema(
     }
 );
 
-userSchema.pre("save", async function (next) {
-    if(!this.isModified("password")) return next()
+userSchema.pre("save", async function () {
+  // next in () gave error of "next is not a function"
+  if (!this.isModified("password")) return; // next()
 
-    this.password = await bcrypt.hash(this.password, 10)
-    // next() // next gave error of "next is not a function"
+  this.password = await bcrypt.hash(this.password, 10);
+  // next() // next gave error of "next is not a function"
 }) // dont use arrow function here, cant get this ref, what to hash, how many rounds
 
 userSchema.methods.isPasswordCorrect = async function (password) {
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+import { refreshAccessToken } from "../controllers/user.controller.js";
 
 const router = Router()
 
@@ -18,5 +20,14 @@ router.route("/register").post(
     registerUser
 )
 // router.route("/login").post(login) for explaination
+
+router.route("/login").post(loginUser)
+
+// secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
+
+router.route("/refresh-token").post(refreshAccessToken)
+
+
 
 export default router
